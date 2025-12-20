9ac8b1d9119ca7f279d804d27ccda7ef4b03fd2b
Rafael Ferreira <rafaelf_s@hotmail.com>
2025-12-20 22:41:46 +0000

    feat: update home page layout to display personalized greeting with first name

--- assets/pages/home.js
+++ assets/pages/home.js
@@ -1,13 +1,16 @@
+import { config } from "../data/config.js";
+
 export const homePage = {
   title: "Home",
   html: /* html */ `
-  <section class="card m-2">
-    <div class="card-header bg-secondary text-white">
-      <h2>Hello,</h2>
-    </div>
-    <div class="card-body">
-      <p>Welcome to your Game Collection App!</p>
-    </div>
+  <section class="m-2">
+      <h2 id="greeting"></h2>
+      <p class="lead">Welcome to your Game Collection App!</p>
   </section>
   `,
+  setup() {
+    const greetingText = document.getElementById("greeting");
+    const firstName = config.getFirstName();
+    greeting.innerText = `Hello, ${firstName}!`;
+  },
 };
