147053c073ab6f0123c4858016f6f3e17ffc5917
Rafael Ferreira <rafaelf_s@hotmail.com>
2025-12-20 22:41:34 +0000

    feat: add getFirstName method to config for retrieving first name from localStorage

--- assets/data/config.js
+++ assets/data/config.js
@@ -2,4 +2,7 @@ export const config = {
   getCurrency() {
     return localStorage.getItem("currency") || "£";
   },
+  getFirstName() {
+    return localStorage.getItem("first-name") || "you";
+  },
 };
